9ec095bc69db103d7de32e4f04f85a53af289005
Anchal Gupta <113814262+AnchalGupta-Work@users.noreply.github.com>
2025-02-12 07:49:41 +0530

    Update vite.config.js

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'router-vendor': ['react-router-dom'],
+          'ui-vendor': ['react-bootstrap', 'bootstrap'],
+          'chart-vendor': ['recharts'],
+          'icon-vendor': ['react-icons', 'bootstrap-icons']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    cors: true
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: true
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'react-bootstrap',
+      'bootstrap',
+      'recharts',
+      'axios'
+    ]
+  }
 })
